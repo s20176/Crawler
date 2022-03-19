5dd2c495dbf6bd9ef9173b0e07be93f9f62066f0
s20176 <s20176@pjwstk.edu.pl>
2022-03-19 19:37:27 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException("Nie przekazano argumentu");
+            }
+            string websiteUrl = args[0];
+
+            var regex = new Regex(@"^(http|https)[:][/][/][a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+            var match=regex.Match(websiteUrl);
+            if (!match.Success)
+            {
+                throw new ArgumentException("Przekazano niepoprawny adres url");
+            }
+            HttpClient httpClient = new HttpClient();
+            try
+            {
+                var response = await httpClient.GetAsync(websiteUrl);
+                
+                Console.WriteLine(response);
+
+                var content = await response.Content.ReadAsStringAsync();
+
+                regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+
+                var matchCollection = regex.Matches(content);
+                var set = new HashSet<string>();
+
+                foreach (Match m in matchCollection)
+                {
+                    set.Add(m.ToString());
+                }
+
+                if (set.Count == 0)
+                {
+                    Console.WriteLine("Nie znaleziono adresów email");
+                }
+                else
+                {
+                    foreach (string s in set)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Błąd w czasie pobierania strony");
+            }
+            httpClient.Dispose();
+
+        }
+    }
+}
